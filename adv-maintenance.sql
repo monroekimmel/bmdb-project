1227d64f65c00b038eff08369b8d542bde2fc88d
monroekimmel <monroekimmel@gmail.com>
2020-08-21 22:02:17 -0400

    added more info

---
+++ adv-maintenance.sql
@@ -0,0 +1,19 @@
+select * from movie;
+
+insert into movie values
+(11, 'What Keeps You Alive', 2018, 'R', 'Colin Minihan');
+
+insert into movie values 
+(12, 'Joker', 2019, 'R', 'Todd Phillips');
+
+insert into movie values
+(13, 'As Above, So Below', 2014, 'R', 'John Erick Dowdle');
+
+insert into movie values
+(14, 'Flubber', 1997, 'PG', 'Les Mayfield');
+
+insert into movie values
+(15, 'The Spongebob Squarepants Movie', 2004, 'PG', 'Stephen Hillenburg');
+
+insert into actor values
+(5, 'Tom', 'Kenny', 'M', '1962-07-13')
